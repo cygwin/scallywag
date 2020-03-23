86289bd2eb7b5c67eee378d7187a23c6f897ad4c
Jon Turney <jon.turney@dronecode.org.uk>
2020-03-23 15:37:08 +0000

    Add 'Authorization:' to notification webhook
    The value checked for is put in the SCALLWAG_AUTH env var, and is
    encrypted with Appveyor for secure storage in .appveyor.yml

--- hook.py
+++ hook.py
@@ -2,8 +2,11 @@
 
 import json
 import logging
+import os
+import re
 import sqlite3
 import subprocess
+import sys
 import time
 import web
 
@@ -21,6 +24,16 @@ def parse_time(s):
 
 class hooks:
     def POST(self):
+        if web.ctx.ip != '138.91.141.243':
+            web.ctx.status = '403 Forbidden'
+            return 'Forbidden'
+
+        auth = web.ctx.env.get('HTTP_AUTHORIZATION', '')
+        auth = re.sub('^Basic ', '', auth)
+        if auth != os.environ.get('SCALLYWAG_AUTH'):
+            web.ctx.status = '401 Unauthorized'
+            return 'Unauthorized'
+
         data = web.data()
         j = json.loads(data)
         with open('last.json', 'w') as f:
@@ -68,6 +81,10 @@ class hooks:
 
 
 if __name__ == '__main__':
+    if 'SCALLYWAG_AUTH' not in os.environ:
+        print('SCALLYWAG_AUTH env var not set')
+        sys.exit(1)
+
     with sqlite3.connect('carpetbag.db') as conn:
         conn.execute('''CREATE TABLE IF NOT EXISTS jobs
                      (id integer primary key, srcpkg text, hash text, user text, status text, logurl text, start_timestamp integer, end_timestamp integer, arches text)''')
