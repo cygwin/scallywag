e8f31044cd06e5eda494f47e4f83743c23a489d8
Jon Turney <jon.turney@dronecode.org.uk>
2021-01-12 14:04:56 +0000

    Install python3-devel for python3-wheel cygclass
    Install python3-devel for python3-wheel cygclass, as it wants to run
    python3-config.

--- analyze.py
+++ analyze.py
@@ -168,7 +168,7 @@ def depends_from_cygport(content):
             (['python2-wheel', 'python-wheel'], ['python2-wheel', 'python2-pip']),
             (['python3'], ['python3']),
             (['python3-distutils'], ['python3-setuptools', 'python3-devel']),
-            (['python3-wheel', 'python-wheel'], ['python36-wheel', 'python36-pip', 'python37-wheel', 'python37-pip', 'python38-wheel', 'python38-pip']),  # done correctly, this needs to understand PYTHON_WHEEL_VERSIONS
+            (['python3-wheel', 'python-wheel'], ['python3-devel', 'python36-wheel', 'python36-pip', 'python37-wheel', 'python37-pip', 'python38-wheel', 'python38-pip']),  # done correctly, this needs to understand PYTHON_WHEEL_VERSIONS
             (['texlive'], ['texlive-collection-basic']),  # to ensure correct run-time dependency generation
             (['xfce4'], ['xfce4-dev-tools']),
             (['xorg'], ['xorg-util-macros']),
