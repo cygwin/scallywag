cygwin/scallywag

commit 39349e36d6ffad5413e4f2b9dd4181526defc53f

Discard comments when analyzing cygport file

analyze.py
@@ -61,6 +61,9 @@ def analyze(repodir):
         f = open(os.path.join(repodir, fn))
         content = f.read()
 
+        # discard comments
+        content = re.sub(r'#.*$', '', content)
+
         # fold any line-continuations
         content = re.sub(r'\\\n', '', content)
 

commit da316002f0bb3e29d7176bbee54376c223e4346b

Fix comment discard in fallback cygport parser
In a regex, $ only matches at the end of the string, not the end of the
line, unless MULTILINE is specified.

Fixes: 39349e36d6ff ("Discard comments when analyzing cygport file")

analyze.py
@@ -124,7 +124,7 @@ def parse_cygport(fn):
         content = f.read()
 
         # discard comments
-        content = re.sub(r'#.*$', '', content)
+        content = re.sub(r'#.*$', '', content, flags=re.MULTILINE)
 
         # fold any line-continuations
         content = re.sub(r'\\\n', '', content)