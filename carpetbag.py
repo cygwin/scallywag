cygwin/scallywag

commit 2123ff09da8a6e1bb7d712ba3be34d6afddd3abc

Extract build_id from job's display title
This makes things more robust against timeouts waiting for a wrf_id to
be assigned.

(There's some suggestion we can retrieve the client_payload of the
workflow run (via the wokflow jobs API) and use that to establish the
binding, but I can't find it)

gh-hook.cgi
@@ -5,6 +5,7 @@ import hashlib
 import hmac
 import json
 import os
+import re
 import sys
 import time
 import traceback
@@ -44,6 +45,12 @@ def process(data):
     u.backend_id = wfr['id']
     u.buildurl = wfr['html_url']
     u.duration = parse_iso8601_time(wfr['updated_at']) - parse_iso8601_time(wfr['created_at'])
+
+    # extract build_id from the title
+    title = wfr['display_title']
+    match = re.search(r'\((.*)\)', title)
+    if match:
+        u.buildnumber = int(match.group(1))
 
     if wfr['conclusion'] == 'success':
         u.status = 'build succeeded'

request_build.py
@@ -139,14 +139,13 @@ def _github_workflow_trigger(package, maintainer, commit, reference, default_tok
     else:
         logging.info("timeout waiting for GitHub to report previous wfr_id")
         print('scallywag: timeout waiting for GitHub to report previous wfr_id')
-        print('scallywag: PLEASE REPORT THIS!')
 
     # strip out any over-quoting in the token, as it's harmful to passing the
     # client_payload into scallywag via the command line
     default_tokens = re.sub(r'[\'"]', r'', default_tokens)
 
     data = {
-        "event_type": "(%s) %s" % (buildnumber, package),  # use this just because it appears as the run name in UI
+        "event_type": "(%s) %s" % (buildnumber, package),  # 'display_title', appears as the run name in UI
         "client_payload": {
             "BUILDNUMBER": buildnumber,
             "PACKAGE": package,
@@ -196,7 +195,6 @@ def _github_workflow_trigger(package, maintainer, commit, reference, default_tok
 
     logging.info("timeout waiting for GitHub to assign a wfr_id")
     print('scallywag: timeout waiting for GitHub to assign a wfr_id')
-    print('scallywag: PLEASE REPORT THIS!')
 
     return 0, None
 

commit c570c8c70fe9554e5858bf7070ebee5920f3c696

Store backend_id we get from GitHub event
This means that if even if we timed-out waiting for it to be assigned
when requesting the build, it is known when we do metadata fetch.

Fixes: 2123ff09da8a ("Extract build_id from job's display title")

TODO
@@ -5,7 +5,5 @@
 - use a venv to avoid depending on user installation of required python modules
 - don't be terrible
 
-- authenticated ways for maintainers to interact with scallywag (via 'ssh cygwin@cygwin.com jobs <subcommand>' ?)
--- cancel a job
--- retry a job
--- deploy a successful job that run without deploy token
+
+- fetcher should check if pending jobs have completed, in case we've missed a job conclusion status notification

carpetbag.py
@@ -30,15 +30,19 @@ def deployable_job(u):
             (u.package != 'playground'))
 
 
+def update_backend_id(u):
+    logging.info(vars(u))
+
+    with sqlite3.connect(dbfile) as conn:
+        conn.execute('UPDATE jobs SET backend_id = ? WHERE id = ?', (u.backend_id, u.buildnumber))
+
+    conn.close()
+
+
 def update_status(u):
     logging.info(vars(u))
 
     with sqlite3.connect(dbfile) as conn:
-        # if the id isn't available, determine it from backend_id
-        if not hasattr(u, 'buildnumber'):
-            cursor = conn.execute('SELECT id FROM jobs WHERE backend_id = ?', (u.backend_id,))
-            u.buildnumber = cursor.fetchone()[0]
-
         conn.execute('UPDATE jobs SET status = ?, logurl = ?, duration = ? WHERE id = ?',
                      (u.status, u.buildurl, u.duration, u.buildnumber))
 

gh-hook.cgi
@@ -81,6 +81,10 @@ def hook():
 
     u = process(data)
     if u:
+        # ensure backend_id is set, if it was previously unknown due to timeout waiting for it to be assigned
+        if hasattr(u, 'buildnumber'):
+            carpetbag.update_backend_id(u)
+
         carpetbag.update_status(u)
 
     return '200 OK', ''