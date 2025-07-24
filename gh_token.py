cygwin/scallywag

commit 31a2d46baaf51b6fd28fd276b5edab911097e2a7

Easier debugging on a test instance
Make it possible to invoke the GitHub repository_dispatch API on a test
instance of this repository for testing and debugging.

Set the GITHUB_DEBUG_OWNER env var to the owner of the test instance,
and provide an access token in the file 'github.token'.

Then invoking post-recieve with a suitable (fake) context will be
directed by the request_build module at the test instance, e.g:

  export GL_REPO="git/cygwin-packages/$PACKAGE"
  export CYGNAME="$MAINTAINER"
  echo $COMMIT $COMMIT $REFERENCE | ./post-receive

gh_token.py
@@ -72,5 +72,20 @@ def fetch_iat():
     return j['token']
 
 
+def fetch_auth():
+    if os.environ['GITHUB_DEBUG_OWNER']:
+        owner = os.environ['GITHUB_DEBUG_OWNER']
+
+        basedir = os.path.dirname(os.path.realpath(__file__))
+        secretfile = os.path.join(basedir, 'github.token')
+        with open(secretfile, 'r') as f:
+            token = f.read().strip()
+    else:
+        owner = 'cygwin'
+        token = fetch_iat()
+
+    return (owner, token)
+
+
 if __name__ == '__main__':
-    print(fetch_iat())
+    print(fetch_auth())

gh.py
@@ -14,7 +14,8 @@ import gh_token
 
 def examine_run_artifacts(wfr_id, u):
     # Retrieve list of workflow run artifacts
-    req = urllib.request.Request('https://api.github.com/repos/cygwin/scallywag/actions/runs/{}/artifacts'.format(wfr_id))
+    (owner, token) = gh_token.fetch_auth()
+    req = urllib.request.Request('https://api.github.com/repos/{}/scallywag/actions/runs/{}/artifacts'.format(owner, wfr_id))
     req.add_header('Accept', 'application/vnd.github.v3+json')
 
     try:
@@ -41,7 +42,7 @@ def examine_run_artifacts(wfr_id, u):
         if a['name'] == 'metadata':
             url = a['archive_download_url']
             req = urllib.request.Request(url)
-            req.add_unredirected_header('Authorization', 'Bearer ' + gh_token.fetch_iat())
+            req.add_unredirected_header('Authorization', 'Bearer ' + token)
 
             # occasionally, the metadata file is 404, despite appearing in the
             # list of artifacts. it seems we need to wait a little while after

request_build.py
@@ -99,9 +99,10 @@ def _github_most_recent_wfr_id():
 
     qs = urllib.parse.urlencode(data)
 
-    req = urllib.request.Request('https://api.github.com/repos/cygwin/scallywag/actions/runs' + '?' + qs)
+    (owner, token) = gh_token.fetch_auth()
+    req = urllib.request.Request('https://api.github.com/repos/%s/scallywag/actions/runs?%s' % (owner, qs))
     req.add_header('Accept', 'application/vnd.github.v3+json')
-    req.add_header('Authorization', 'Bearer ' + gh_token.fetch_iat())
+    req.add_header('Authorization', 'Bearer ' + token)
 
     try:
         response = urllib.request.urlopen(req)
@@ -156,10 +157,11 @@ def _github_workflow_trigger(package, maintainer, commit, reference, default_tok
         }
     }
 
-    req = urllib.request.Request('https://api.github.com/repos/cygwin/scallywag/dispatches')
+    (owner, token) = gh_token.fetch_auth()
+    req = urllib.request.Request('https://api.github.com/repos/%s/scallywag/dispatches' % owner)
 
     req.add_header('Accept', 'application/vnd.github.v3+json')
-    req.add_header('Authorization', 'Bearer ' + gh_token.fetch_iat())
+    req.add_header('Authorization', 'Bearer ' + token)
 
     try:
         response = urllib.request.urlopen(req, data=json.dumps(data).encode('utf-8'))
@@ -253,10 +255,11 @@ def request_build(commit, reference, package, maintainer, tokens=''):
 
 
 def _github_workflow_cancel(wfr_id):
-    req = urllib.request.Request('https://api.github.com/repos/cygwin/scallywag/actions/runs/{}/cancel'.format(wfr_id), method='POST')
+    (owner, token) = gh_token.fetch_auth()
+    req = urllib.request.Request('https://api.github.com/repos/{}/scallywag/actions/runs/{}/cancel'.format(owner, wfr_id), method='POST')
 
     req.add_header('Accept', 'application/vnd.github.v3+json')
-    req.add_header('Authorization', 'Bearer ' + gh_token.fetch_iat())
+    req.add_header('Authorization', 'Bearer ' + token)
 
     try:
         response = urllib.request.urlopen(req)

commit 279a90503d44a80fbb92849eca1945ec6e23b1f2

Make it work when GITHUB_DEBUG_OWNER isn't set
Fixes: 31a2d46baaf5 ("Easier debugging on a test instance")

gh_token.py
@@ -73,7 +73,7 @@ def fetch_iat():
 
 
 def fetch_auth():
-    if os.environ['GITHUB_DEBUG_OWNER']:
+    if 'GITHUB_DEBUG_OWNER' in os.environ:
         owner = os.environ['GITHUB_DEBUG_OWNER']
 
         basedir = os.path.dirname(os.path.realpath(__file__))